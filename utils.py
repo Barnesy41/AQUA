5f66f988149ca9bbfaf129ce401bdd153316d76a
Barnesey41 <obarnes23@hotmail.co.uk>
2022-11-07 18:38:46 +0000

    Added exception check subroutines

--- utils.py
+++ utils.py
@@ -4,8 +4,13 @@
 
 
 def sumvalues(values):
-    """Your documentation goes here"""    
-    ## Your code goes here
+    """Your documentation goes here""" 
+    sum = 0   
+    for item in values:
+        sum += item
+    
+    return sum 
+    ##TODO raise exception for non-numerical values
 
 
 def maxvalue(values):
@@ -26,3 +31,16 @@ def meannvalue(values):
 def countvalue(values,xw):
     """Your documentation goes here"""    
     ## Your code goes here
+    
+def checkExceptionInteger(value):
+    if(not type(value) is int):
+        raise Exception("Value of type: " + str(type(value)) + " found. Value of type: " + str(type(1)) + " expected.")
+    
+def checkExceptionString(value):
+    if(not type(value) is str):
+        raise Exception("Value of type: " + str(type(value)) + " found. Value of type: " + str(type("1")) + " expected.")
+        
+def checkExceptionBool(value):
+    if(not type(value) is bool):
+        raise Exception("Value of type: " + str(type(value)) + " found. Value of type: " + str(type(True)) + " expected.")
+
